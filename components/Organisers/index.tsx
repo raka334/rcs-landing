573e79827593d3114ad73179c8788cd5eaf275c5
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2024-01-06 14:45:16 +0530

    mod: update content

--- components/Organisers/index.tsx
+++ components/Organisers/index.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 function Organisers() {
     return (
         <section className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 pt-28">
-            <br></br>
-            <br></br>
-            <br></br>
             <h2 className="text-5xl lg:text-8xl text-primary font-bold">Organisers</h2>
             <div className="list w-full gap-8 lg:gap-16 p-8 lg:p-10 max-w-4xl mx-auto">
                     <div key="ee" className="flex flex-col items-center lg:px-5">
--- components/ResourcePersons/index.tsx
+++ components/ResourcePersons/index.tsx
@@ -5,9 +5,6 @@ import rersourcePersons from "@/config/resourcepersons.json";
 function RersourcePersons() {
     return (
         <section className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 pt-28">
-            <br></br>
-            <br></br>
-            <br></br>
             <h2 className="text-5xl lg:text-8xl text-primary font-bold">Advisory Commitee</h2>
             <div className="list grid grid-cols-2 lg:grid-cols-3 w-full gap-8 lg:gap-16 p-8 lg:p-10 max-w-4xl mx-auto">
                 {rersourcePersons.map((member) => (
