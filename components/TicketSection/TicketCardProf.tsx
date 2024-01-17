6922889fbfb2738baa4564285054c9c88057ec75
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2024-01-17 15:23:05 +0530

    fix: merge conflict

--- components/TicketSection/TicketCardProf.tsx
+++ components/TicketSection/TicketCardProf.tsx
@@ -3,7 +3,6 @@ import Button from '../ui/Button'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 
-<<<<<<< HEAD
 function TicketCard({
     setTicketType,
     setTicketBookingOpen,
@@ -13,56 +12,42 @@ function TicketCard({
 }) {
     const router = useRouter()
     return (
-        <div className="card w-full max-w-sm bg-white text-black p-5">
-            <p className="font-bold text-4xl">Professional Pass</p>
-            <p className="mt-8 text-lg">
-                For working professionals and students from other colleges.
-            </p>
+        <>
+            <div className="card w-full max-w-sm bg-white text-black p-5">
+                <p className="font-bold text-4xl">Professional Pass</p>
+                <p className="mt-8 text-lg">
+                    For working professionals and students from other colleges.
+                </p>
 
-            <div className="mt-8 text-lg">
-                <p className="text-primary">Includes: </p>
-                <div className="flex flex-col gap-2">
-                    <p>
-                        {' '}
-                        - Welcome Kit Includes: 1 x Notepad-Pen, 1 x ID-Lanyard,
-                        Stickers
-                    </p>
-=======
-function TicketCard() {
-  return (
-      <div className="card w-full max-w-sm bg-white text-black p-5 rounded-lg">
-          <p className="font-bold text-4xl">
-            Professional Pass
-          </p>
-          <p className="mt-8 text-lg">
-            For working professionals and students from other colleges.
-          </p>
-
-          <div className="mt-8 text-lg">
-              <p className="text-primary">Includes: </p>
-              <div className="flex flex-col gap-2">
-                    <p> - 1 x ID-Lanyard</p>
->>>>>>> f1d45627580b98e6969567d25a9e94c86d56d779
-                    <p> - Access to CTF</p>
-                    <p> - 2 x Meals Provided</p>
-                    <p> - Complimentry Swags</p>
-                    <p> - Conference Attendee Certificate</p>
+                <div className="mt-8 text-lg">
+                    <p className="text-primary">Includes: </p>
+                    <div className="flex flex-col gap-2">
+                        <p>
+                            {' '}
+                            - Welcome Kit Includes: 1 x Notepad-Pen, 1 x ID-Lanyard,
+                            Stickers
+                        </p>
+                        <p> - Access to CTF</p>
+                        <p> - 2 x Meals Provided</p>
+                        <p> - Complimentry Swags</p>
+                        <p> - Conference Attendee Certificate</p>
+                    </div>
+                </div>
+                <div className="button mt-8">
+                    <Button
+                        size={'full'}
+                        onClick={() => {
+                            router.push(
+                                `${process.env.NEXT_PUBLIC_PAYMENT_WEBSITE}/professional_pass`
+                            )
+                        }}
+                        className="bg-red-950"
+                    >
+                        Rs. 500
+                    </Button>
                 </div>
             </div>
-            <div className="button mt-8">
-                <Button
-                    size={'full'}
-                    onClick={() => {
-                        router.push(
-                            `${process.env.NEXT_PUBLIC_PAYMENT_WEBSITE}/professional_pass`
-                        )
-                    }}
-                    className="bg-red-950"
-                >
-                    Rs. 500
-                </Button>
-            </div>
-        </div>
+        </>
     )
 }
 
